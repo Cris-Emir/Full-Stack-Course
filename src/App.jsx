8c4c83bdb840d926e12ee5dd926b661decf5ad40
Cris-Emir <cdelacruz@espoilenergy.com>
2024-10-15 14:19:57 -0600

    Re-renderizado de la pagina

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,8 @@
-const Hello = ({ name, age }) => {
-  // const name = props.name
-  // const age = props.age
-
-  // const { name, age } = props;
-
-  const bornYear = () => new Date().getFullYear() - age;
-
+const App = (props) => {
+  const {counter} = props
   return (
-    <div>
-      <p>
-        Hello {name}, you are {age} years old
-      </p>
-      <p>So you were probably born in {bornYear()}</p>
-    </div>
-  );
-};
-const App = () => {
-  const name = "Peter";
-  const age = 10;
+    <div>{counter}</div>
+  )
+}
 
-  return (
-    <div>
-      <h1>Greetings</h1>
-      <Hello name="Maya" age={26 + 10} />
-      <Hello name={name} age={age} />
-    </div>
-  );
-};
-
-export default App;
+export default App
