3195f75e7cfaa62c10f29de85704b18afc2bdd48
Cris-Emir <cdelacruz@espoilenergy.com>
2024-10-16 08:00:11 -0600

    Componente con estado

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,11 @@
-import ReactDOM from "react-dom/client";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
 
-import App from "./App";
 
-let counter = 1;
 
-const refresh = () => {
-  ReactDOM.createRoot(document.getElementById("root")).render(
-    <App counter={counter} />
-  );
-};
-
-setInterval(() => {
-  refresh()
-  counter += 1
-}, 1000)
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
