4af6bd1cff642863d30c7ab765d5910aeb19ff45
Cris-Emir <cdelacruz@espoilenergy.com>
2024-10-16 08:32:33 -0600

    Control de Eventos

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,22 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 const App = () => {
-  const [ counter, setCounter ] = useState(0)
+  const [counter, setCounter] = useState(0);
 
-  setTimeout(
-    () => setCounter(counter + 1),
-    1000
-  )
-
-
-  console.log('rendering...', counter)
+  const incraseByOne = () => {
+    setCounter(counter + 1);
+  };
+  const setToZero = () => {
+    setCounter(0);
+  };
 
   return (
-    <div>{counter}</div>
-  )
-}
+    <div>
+      <div>{counter}</div>
+      <button onClick={incraseByOne}>plus</button>
+      <button onClick={setToZero}>zero</button>
+    </div>
+  );
+};
 
-export default App
+export default App;
