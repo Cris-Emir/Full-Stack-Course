17436de4ce7645126c20ad6564ade9d7d8f9fe3d
Cris-Emir <cdelacruz@espoilenergy.com>
2024-10-17 14:41:22 -0600

    Estado Complejo

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,23 @@
 import { useState } from "react";
-0;
-const Display = ({ counter }) => <div>{counter}</div>;
-
-// Componente boton donde se recibe el props donde recibe el evento Onclick y un elemento text
-const Button = ({ onSmash, text }) => <button onClick={onSmash}>{text}</button>;
 
 const App = () => {
-  const [counter, setCounter] = useState(0);
-  console.log("rendering with counter value...", counter);
+  const [clicks, setClicks] = useState({
+    left: 0,
+    right: 0,
+  });
 
-  const incraseByOne = () => {
-    console.log("increasing, value before", counter);
-    setCounter(counter + 1);
-  };
-  const decreaseByOne = () => {
-    console.log("decreasing, value before", counter);
-    setCounter(counter - 1);
-  };
-  const setToZero = () => {
-    console.log("reseting to zero, value before", counter);
-    setCounter(0);
-  };
+const handleLeftClick = () =>
+  setClicks({ ...clicks, left: clicks.left + 1 })
+
+const handleRightClick = () =>
+  setClicks({ ...clicks, right: clicks.right + 1 })
 
   return (
     <div>
-      <Display counter={counter}></Display>
-      <Button onClick={incraseByOne} text="plus"></Button>
-      <Button onClick={setToZero} text="zero"></Button>
-      <Button onClick={decreaseByOne} text="minus"></Button>
+      {clicks.left}
+      <button onClick={handleLeftClick}>left</button>
+      <button onClick={handleRightClick}>right</button>
+      {clicks.right}
     </div>
   );
 };
