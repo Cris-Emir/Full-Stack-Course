870b624515bc4195c93e911b0a63d84d6e4ccd65
Cris-Emir <cdelacruz@espoilenergy.com>
2024-10-17 12:53:01 -0600

    Refactorizacion de los componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,33 @@
 import { useState } from "react";
 0;
-const Display = (props) => {
-  return <div>{props.counter}</div>;
-};
+const Display = ({ counter }) => <div>{counter}</div>;
 
-// Componente boton donde se recibe el props donde recibe el evento Onclick y un elemento text 
-const Button = (props) => {
-  return <button onClick={props.onClick}>{props.text}</button>;
-};
+// Componente boton donde se recibe el props donde recibe el evento Onclick y un elemento text
+const Button = ({ onSmash, text }) => <button onClick={onSmash}>{text}</button>;
 
 const App = () => {
   const [counter, setCounter] = useState(0);
+  console.log("rendering with counter value...", counter);
 
   const incraseByOne = () => {
+    console.log("increasing, value before", counter);
     setCounter(counter + 1);
   };
   const decreaseByOne = () => {
+    console.log("decreasing, value before", counter);
     setCounter(counter - 1);
   };
   const setToZero = () => {
+    console.log("reseting to zero, value before", counter);
     setCounter(0);
   };
 
   return (
     <div>
       <Display counter={counter}></Display>
-      <Button onClick={incraseByOne} text='plus'></Button>
-      <Button onClick={setToZero} text='zero'></Button>
-      <Button onClick={decreaseByOne} text='minus'></Button>
+      <Button onClick={incraseByOne} text="plus"></Button>
+      <Button onClick={setToZero} text="zero"></Button>
+      <Button onClick={decreaseByOne} text="minus"></Button>
     </div>
   );
 };
