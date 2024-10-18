3f5c9d93f17832b578cd1e7aceecddf66a061094
Cris-Emir <cdelacruz@espoilenergy.com>
2024-10-18 09:01:49 -0600

    Renderizado condicional

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,47 @@
 import { useState } from "react";
 
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return <div>The app is used by pressing the buttons</div>;
+  }
+  return <div>button press history: {props.allClicks.join(" ")}</div>;
+};
+
+const Button = ({ handleClick, text }) => {
+  <button onClick={handleClick}>
+    {text}
+  </button>
+}
+
 const App = () => {
-  const [left, setLeft] = useState(0)
-  const [right, setRight] = useState(0)
-
-  const [allClicks, setAll] = useState([])
-
-  const [total, setTotal] = useState(0)
-
+  const [left, setLeft] = useState(0);
+  const [right, setRight] = useState(0);
+  const [allClicks, setAll] = useState([]);
 
   const handleLeftClick = () => {
-    setAll(allClicks.concat('L'))
+    setAll(allClicks.concat("L"));
     // console.log('left before', left)
-    const updateLeft = left + 1
-    setLeft(updateLeft)
+    const updateLeft = left + 1;
+    setLeft(updateLeft);
     // console.log('left after', left)
-    setTotal(updateLeft + right)
-  }
-
+    setRight(updateLeft + right);
+  };
 
   const handleRightClick = () => {
-    setAll(allClicks.concat('R'))
-    setRight(right + 1)
-    setTotal(left + right)
-  }
+    setAll(allClicks.concat("R"));
+    setRight(right + 1);
+  };
 
   return (
     <div>
       {left}
-      <button onClick={handleLeftClick}>left</button>
-      <button onClick={handleRightClick}>right</button>
+      <Button handleClick={handleLeftClick} text='left'/>
+      <Button handleClick={handleRightClick} text='right'/>
       {right}
 
-      <p>register clicks: {allClicks.join('')}</p>
-      <p>total {total}</p>
+      <History allClicks={allClicks} />
     </div>
-  )
-}
+  );
+};
 
 export default App;
