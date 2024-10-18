4db54b2d797ade7b99409b8693e558edf513088f
Cris-Emir <cdelacruz@espoilenergy.com>
2024-10-18 09:53:00 -0600

    Una funcion que devuelve una funcion

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,39 @@
 import { useState } from "react";
 
-const History = (props) => {
-  if (props.allClicks.length === 0) {
-    return <div>The app is used by pressing the buttons</div>;
-  }
-  return <div>button press history: {props.allClicks.join(" ")}</div>;
-};
-
-const Button = ({ handleClick, text }) => {
-  <button onClick={handleClick}>
-    {text}
-  </button>
-}
-
 const App = () => {
-  const [left, setLeft] = useState(0);
-  const [right, setRight] = useState(0);
-  const [allClicks, setAll] = useState([]);
+  const [value, setValue] = useState(10);
 
-  const handleLeftClick = () => {
-    setAll(allClicks.concat("L"));
-    // console.log('left before', left)
-    const updateLeft = left + 1;
-    setLeft(updateLeft);
-    // console.log('left after', left)
-    setRight(updateLeft + right);
-  };
+  const setToValue = (newValue) => {
+    console.log('value now', newValue)  // imprime el nuevo valor en la consola
+    setValue(newValue)
+  }
 
-  const handleRightClick = () => {
-    setAll(allClicks.concat("R"));
-    setRight(right + 1);
-  };
+
+  const hello = (who) => {
+    const handler = () => {
+      console.log('hello', who)
+    }
+    return handler
+  }
 
   return (
     <div>
-      {left}
-      <Button handleClick={handleLeftClick} text='left'/>
-      <Button handleClick={handleRightClick} text='right'/>
-      {right}
+      {value}
+      <button onClick={() => setToValue(1000)}>
+        thousand
+      </button>
+      <button onClick={() => setToValue(0)}>
+        reset
+      </button>
+      <button onClick={() => setToValue(value + 1)}>
+        increment
+      </button>
+      
+      {value}
+      <button onClick={hello('world')}>button</button>
+      <button onClick={hello('react')}>button</button>
+      <button onClick={hello('function')}>button</button>
 
-      <History allClicks={allClicks} />
     </div>
   );
 };
