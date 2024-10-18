520f120ee6f112098e8453c1e4ebc533d8368af1
Cris-Emir <cdelacruz@espoilenergy.com>
2024-10-18 10:02:24 -0600

    Eventos a componentes hijos

--- src/App.jsx
+++ src/App.jsx
@@ -4,36 +4,20 @@ const App = () => {
   const [value, setValue] = useState(10);
 
   const setToValue = (newValue) => {
-    console.log('value now', newValue)  // imprime el nuevo valor en la consola
-    setValue(newValue)
-  }
+    console.log("value now", newValue); // imprime el nuevo valor en la consola
+    setValue(newValue);
+  };
 
-
-  const hello = (who) => {
-    const handler = () => {
-      console.log('hello', who)
-    }
-    return handler
-  }
+  const Button = (props) => (
+    <button onClick={props.handleClick}>{props.text}</button>
+  );
 
   return (
     <div>
       {value}
-      <button onClick={() => setToValue(1000)}>
-        thousand
-      </button>
-      <button onClick={() => setToValue(0)}>
-        reset
-      </button>
-      <button onClick={() => setToValue(value + 1)}>
-        increment
-      </button>
-      
-      {value}
-      <button onClick={hello('world')}>button</button>
-      <button onClick={hello('react')}>button</button>
-      <button onClick={hello('function')}>button</button>
-
+      <Button handleClick={() => setToValue(1000)} text="thousand" />
+      <Button handleClick={() => setToValue(0)} text="reset" />
+      <Button handleClick={() => setToValue(value + 1)} text="increment" />
     </div>
   );
 };
