b680f29c17f3e06ec066d02be8c22739344a2252
Cris-Emir <cdelacruz@espoilenergy.com>
2024-12-05 22:05:59 -0600

    Ejercicios 2.1.-2.5.

---
+++ src/components/Course.jsx
@@ -0,0 +1,25 @@
+const Course = ({ course }) => {
+  return (
+    <div>
+      {course.map((course) => (
+        <div key={course.id}>
+          <h1>{course.name}</h1>
+          {course.parts.map((part) => (
+            <p key={part.id}>
+              {part.name} {part.exercises}
+            </p>
+          ))}
+          <p>
+            <strong>
+              total of{" "}
+              {course.parts.reduce((sum, part) => sum + part.exercises, 0)}{" "}
+              exercises
+            </strong>
+          </p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Course;
